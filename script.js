f9ea4368d1d4e0529a616d9f87dd08c652ad7c05
kawakaze0815 <130416324+kawakaze0815@users.noreply.github.com>
2023-12-12 10:15:56 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-// script.js
-document.addEventListener('DOMContentLoaded', function () {
-    // getElementById を使用して要素にアクセス
-    var contentElement = document.getElementById('content');
-
-    // 要素の内容を変更
-    contentElement.innerHTML += '<p>GitHub makes web development fun!</p>';
-
-    // その他の操作を追加
-    // ...
-});
+// No JavaScript functionality in this example
