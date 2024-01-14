a9e5c3cb7eec5b3e2ba9ceba2aee2e0224ef38c6
kawakaze0815 <130416324+kawakaze0815@users.noreply.github.com>
2024-01-14 12:19:02 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-// No JavaScript functionality in this example
+function changeURL() {
+    document.getElementById('inuhenkou').src = '新しい画像のURL'; // ここに新しい画像のURLを指定
+}
+
+function restoreImage() {
+    document.getElementById('inuhenkou').src = './image/inu1.jpg';
+}
